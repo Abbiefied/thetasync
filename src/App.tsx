7646666002a3a94b14ffdefe6dc20d42b26652fc
Abbiefied <88349524+Abbiefied@users.noreply.github.com>
2025-06-30 17:35:12 +0100

    Fix Header and Footer display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/auth/ProtectedRoute';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import Toast from './components/common/Toast';
 
 // Pages
 import Landing from './pages/Landing';
@@ -19,64 +22,69 @@ import TestSupabase from './pages/TestSupabase';
 
 function App() {
   return (
-    <div className="min-h-screen bg-neutral-50">
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/test-supabase" element={<TestSupabase />} />
-        
-        {/* Protected routes */}
-        <Route path="/onboarding" element={
-          <ProtectedRoute>
-            <Onboarding />
-          </ProtectedRoute>
-        } />
-        <Route path="/homepage" element={
-          <ProtectedRoute>
-            <Homepage />
-          </ProtectedRoute>
-        } />
-        <Route path="/discover" element={
-          <ProtectedRoute>
-            <Discover />
-          </ProtectedRoute>
-        } />
-        <Route path="/my-groups" element={
-          <ProtectedRoute>
-            <MyGroups />
-          </ProtectedRoute>
-        } />
-        <Route path="/create-group" element={
-          <ProtectedRoute>
-            <CreateGroup />
-          </ProtectedRoute>
-        } />
-        <Route path="/join-group/:groupId" element={
-          <ProtectedRoute>
-            <JoinGroup />
-          </ProtectedRoute>
-        } />
-        <Route path="/group/:groupId" element={
-          <ProtectedRoute>
-            <GroupWorkspace />
-          </ProtectedRoute>
-        } />
-        <Route path="/resources" element={
-          <ProtectedRoute>
-            <Resources />
-          </ProtectedRoute>
-        } />
-        <Route path="/leaderboard" element={
-          <ProtectedRoute>
-            <Leaderboard />
-          </ProtectedRoute>
-        } />
-        
-        {/* Catch all route */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+    <div className="min-h-screen bg-neutral-50 font-inter">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/test-supabase" element={<TestSupabase />} />
+          
+          {/* Protected routes */}
+          <Route path="/onboarding" element={
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          } />
+          <Route path="/homepage" element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          } />
+          <Route path="/discover" element={
+            <ProtectedRoute>
+              <Discover />
+            </ProtectedRoute>
+          } />
+          <Route path="/my-groups" element={
+            <ProtectedRoute>
+              <MyGroups />
+            </ProtectedRoute>
+          } />
+          <Route path="/create-group" element={
+            <ProtectedRoute>
+              <CreateGroup />
+            </ProtectedRoute>
+          } />
+          <Route path="/group/:id/join" element={
+            <ProtectedRoute>
+              <JoinGroup />
+            </ProtectedRoute>
+          } />
+          <Route path="/group/:id" element={
+            <ProtectedRoute>
+              <GroupWorkspace />
+            </ProtectedRoute>
+          } />
+          <Route path="/resources" element={
+            <ProtectedRoute>
+              <Resources />
+            </ProtectedRoute>
+          } />
+          <Route path="/leaderboard" element={
+            <ProtectedRoute>
+              <Leaderboard />
+            </ProtectedRoute>
+          } />
+          
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+      <Footer />
+      <Toast />
     </div>
   );
 }
