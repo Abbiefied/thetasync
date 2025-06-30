613418fd67af35f59bd6b10d728cb9ef89d233f8
Abbiefied <88349524+Abbiefied@users.noreply.github.com>
2025-06-30 17:29:03 +0100

    Fix AuthProvider context error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { AppProvider } from './context/AppContext';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 
 // Pages
@@ -21,71 +19,65 @@ import TestSupabase from './pages/TestSupabase';
 
 function App() {
   return (
-    <AuthProvider>
-      <AppProvider>
-        <Router>
-          <div className="min-h-screen bg-neutral-50">
-            <Routes>
-              {/* Public routes */}
-              <Route path="/" element={<Landing />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/test-supabase" element={<TestSupabase />} />
-              
-              {/* Protected routes */}
-              <Route path="/onboarding" element={
-                <ProtectedRoute>
-                  <Onboarding />
-                </ProtectedRoute>
-              } />
-              <Route path="/homepage" element={
-                <ProtectedRoute>
-                  <Homepage />
-                </ProtectedRoute>
-              } />
-              <Route path="/discover" element={
-                <ProtectedRoute>
-                  <Discover />
-                </ProtectedRoute>
-              } />
-              <Route path="/my-groups" element={
-                <ProtectedRoute>
-                  <MyGroups />
-                </ProtectedRoute>
-              } />
-              <Route path="/create-group" element={
-                <ProtectedRoute>
-                  <CreateGroup />
-                </ProtectedRoute>
-              } />
-              <Route path="/join-group/:groupId" element={
-                <ProtectedRoute>
-                  <JoinGroup />
-                </ProtectedRoute>
-              } />
-              <Route path="/group/:groupId" element={
-                <ProtectedRoute>
-                  <GroupWorkspace />
-                </ProtectedRoute>
-              } />
-              <Route path="/resources" element={
-                <ProtectedRoute>
-                  <Resources />
-                </ProtectedRoute>
-              } />
-              <Route path="/leaderboard" element={
-                <ProtectedRoute>
-                  <Leaderboard />
-                </ProtectedRoute>
-              } />
-              
-              {/* Catch all route */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </div>
-        </Router>
-      </AppProvider>
-    </AuthProvider>
+    <div className="min-h-screen bg-neutral-50">
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/test-supabase" element={<TestSupabase />} />
+        
+        {/* Protected routes */}
+        <Route path="/onboarding" element={
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        } />
+        <Route path="/homepage" element={
+          <ProtectedRoute>
+            <Homepage />
+          </ProtectedRoute>
+        } />
+        <Route path="/discover" element={
+          <ProtectedRoute>
+            <Discover />
+          </ProtectedRoute>
+        } />
+        <Route path="/my-groups" element={
+          <ProtectedRoute>
+            <MyGroups />
+          </ProtectedRoute>
+        } />
+        <Route path="/create-group" element={
+          <ProtectedRoute>
+            <CreateGroup />
+          </ProtectedRoute>
+        } />
+        <Route path="/join-group/:groupId" element={
+          <ProtectedRoute>
+            <JoinGroup />
+          </ProtectedRoute>
+        } />
+        <Route path="/group/:groupId" element={
+          <ProtectedRoute>
+            <GroupWorkspace />
+          </ProtectedRoute>
+        } />
+        <Route path="/resources" element={
+          <ProtectedRoute>
+            <Resources />
+          </ProtectedRoute>
+        } />
+        <Route path="/leaderboard" element={
+          <ProtectedRoute>
+            <Leaderboard />
+          </ProtectedRoute>
+        } />
+        
+        {/* Catch all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { AppProvider } from './context/AppContext';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <AppProvider>
+        <Router>
+          <App />
+        </Router>
+      </AppProvider>
+    </AuthProvider>
   </StrictMode>
 );
